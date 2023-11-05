de8f6b4ab98feac2941a6edc07ee726d501df1e0
ChefServiceInfo <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>
2023-11-05 16:53:38 +0100

    Refactoring to eliminate the Dollar/Franc duplication, by replacing the direct references to the previous classes in the code with the Money class and his two factory methods.

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,8 +3,18 @@ package guru.springframework;
 /**
  * Created by Rami SAHRAOUI on 05/11/2023
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -11,31 +11,31 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
     @Test
     void testDollarMultiplication() {
-        Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
+        Money five = Money.dollar(5);
+        Money product = five.times(2);
+        assertEquals(Money.dollar(10), product);
         product = five.times(3);;
-        assertEquals(new Dollar(15), product);
+        assertEquals(Money.dollar(15), product);
     }
 
     @Test
     void testDollarEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(8));
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
     @Test
     void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        Franc product = five.times(2);
-        assertEquals(new Franc(10), product);
+        Money five = Money.franc(5);
+        Money product = five.times(2);
+        assertEquals(Money.franc(10), product);
         product = five.times(3);;
-        assertEquals(new Franc(15), product);
+        assertEquals(Money.franc(15), product);
     }
 
     @Test
     void testFrancEquality() {
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(5), new Franc(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(8));
     }
 }
