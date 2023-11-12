5bef1935e3d84e37abec5919874bc882366d6296
sahraoui-rami <149308696+sahraoui-rami@users.noreply.github.com>
2023-11-12 11:37:02 +0100

    Safe deleting Dollar and Franc classes, and refactoring the test methods.

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -10,33 +10,25 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
  */
 public class MoneyTest {
     @Test
-    void testDollarMultiplication() {
-        Money five = Money.dollar(5);
-        Money product = five.times(2);
-        assertEquals(Money.dollar(10), product);
-        product = five.times(3);
-        assertEquals(Money.dollar(15), product);
+    void testMultiplication() {
+        Money fiveD = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveD.times(2));
+        assertEquals(Money.dollar(15), fiveD.times(3));
+
+        Money fiveF = Money.franc(5);
+        assertEquals(Money.franc(10), fiveF.times(2));
+        assertEquals(Money.franc(15), fiveF.times(3));
     }
 
     @Test
-    void testDollarEquality() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-        assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
-    @Test
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        Money product = five.times(2);
-        assertEquals(Money.franc(10), product);
-        product = five.times(3);
-        assertEquals(Money.franc(15), product);
-    }
 
-    @Test
-    void testFrancEquality() {
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.franc(5), Money.franc(8));
+
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
